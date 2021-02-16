140891c2631dd2fb1f517eedc7af46e76fc1bc7a
Krzysiek Swol <krzysiek.swol@gmail.com>
2021-02-16 08:08:25 +0100

    change to EN language

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class Weather {
   }
   showWeather() {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${process.env.WEATHER_API_KEY}&lang=pl`
+      `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${process.env.WEATHER_API_KEY}&lang=en`
     )
       .then((resp) => resp.json())
       .then((data) => {
@@ -17,9 +17,9 @@ class Weather {
         image.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
         document.getElementById(
           `showWx${this.citycode}`
-        ).innerHTML = `Aktualna temperatura wynosi : ${(
+        ).innerHTML = `Current temperature : ${(
           data.main.temp - 273.15
-        ).toFixed(1)} &#x2103, a odczuwalna to ${(
+        ).toFixed(1)} &#x2103, perceived temperature ${(
           data.main.feels_like - 273.15
         ).toFixed(1)} &#x2103`;
         document.getElementById(`icon${this.citycode}`).appendChild(image);
